39114688236d6dc032db5c772cd6575d5531b535
baji123 <shaikbaji123@gmail.com>
2017-05-17 00:19:44 +0530

    fills added

---
+++ src/main/java/baji2/com/in/App.java
@@ -0,0 +1,13 @@
+package baji2.com.in;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
